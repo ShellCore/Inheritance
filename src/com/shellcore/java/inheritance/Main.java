1492fdf969309d7ffd2ec18926032460d3af7511
Cesar Morales Garduño <shell.one.core@gmail.com>
2017-05-08 11:28:20 -0500

    Creación de main para las pruebas de la implementación de la herencia.

---
+++ src/com/shellcore/java/inheritance/Main.java
@@ -0,0 +1,15 @@
+package com.shellcore.java.inheritance;
+
+/**
+ * Created by Cesar. 04/05/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Car ferrari = new F1Car(320);
+        Car coupe = new Car(220);
+
+        ferrari.getDescription();
+        coupe.getDescription();
+    }
+}
